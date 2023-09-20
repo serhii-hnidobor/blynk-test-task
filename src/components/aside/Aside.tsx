02fe9e315782c8b72f2a4e47426c65568de63219
Serhii Hnidobor <serhii.hnidobor@tjhelpers.com>
2023-09-21 02:45:31 +0300

    * update aside text content

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -3,7 +3,8 @@ import "./styles.css";
 function Aside() {
   return (
     <aside className="aside">
-      <h2 className="aside__heading">DAYRY APP</h2>
+      <h2 className="aside__heading">dayry app</h2>
+      <h3 className="aside__sub-heading">Comment whit no sense</h3>
     </aside>
   );
 }
